566014e88c69e9e89b66dcea6d943a565d760419
AlejandraMoreanoV <161619454+AlejandraMoreanoV@users.noreply.github.com>
2024-05-26 14:16:49 -0500

    Primer commit del tercer parcial.

---
+++ src/main/java/com/example/demo/mapper/IUsuarioMapper.java
@@ -0,0 +1,9 @@
+package com.example.demo.mapper;
+
+import com.example.demo.modelo.Usuario;
+import com.example.demo.persistencia.EntityUsuario;
+
+public interface IUsuarioMapper {
+    Usuario toDto (EntityUsuario entityUsuario);
+    EntityUsuario toEntity (Usuario usuario);
+}
